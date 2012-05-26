28c56e74739c35f3a74cef0690dad3b3bbb94b2c
Ben Bangert <ben@groovie.org>
2012-05-25 18:01:59 -0700

    Fix RTD build.

--- setup.py
+++ setup.py
@@ -24,8 +24,6 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:
     install_requires.extend([
     ])
-else:
-    install_requires = tests_require
 
 setup(
     name='kazoo',
