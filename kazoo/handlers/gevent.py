00af756166ca5aa77a5f82cbf6eb99d51904bf54
Ben Bangert <ben@groovie.org>
2012-05-28 14:27:27 -0700

    Use pipe trick to ensure gevent queue's wake up when set from a separate thread.

--- kazoo/handlers/gevent.py
+++ kazoo/handlers/gevent.py
@@ -1,13 +1,37 @@
 """gevent Handler"""
 from __future__ import absolute_import
 
+import fcntl
+import os
+
 import gevent
 import gevent.event
-import gevent.queue
+from gevent.queue import Empty
+from gevent.queue import Queue
 from zope.interface import implementer
 
 from kazoo.interfaces import IAsyncResult
 from kazoo.interfaces import IHandler
+
+
+# Simple wrapper to os.pipe() - but sets to non-block
+def _pipe():
+    r, w = os.pipe()
+    fcntl.fcntl(r, fcntl.F_SETFL, os.O_NONBLOCK)
+    fcntl.fcntl(w, fcntl.F_SETFL, os.O_NONBLOCK)
+    return r, w
+
+_core_pipe_read, _core_pipe_write = _pipe()
+
+
+def _core_pipe_read_callback(event, evtype):
+    try:
+        os.read(event.fd, 1)
+    except:
+        pass
+
+gevent.core.event(gevent.core.EV_READ | gevent.core.EV_PERSIST,
+                 _core_pipe_read, _core_pipe_read_callback).add()
 
 
 @implementer(IAsyncResult)
@@ -22,11 +46,17 @@ class AsyncResult(gevent.event.AsyncResult):
             lambda: gevent.event.AsyncResult.set(self, value)
         )
 
+        # Wake gevent wait/gets
+        os.write(_core_pipe_write, '\0')
+
     def set_exception(self, exception):
         # Proxy the set_exception call to the gevent thread
         self._handler.completion_queue.put(
             lambda: gevent.event.AsyncResult.set_exception(self, exception)
         )
+
+        # Wake gevent wait/gets
+        os.write(_core_pipe_write, '\0')
 
 
 @implementer(IHandler)
@@ -34,9 +64,9 @@ class SequentialGeventHandler(object):
     timeout_exception = gevent.event.Timeout
 
     def __init__(self):
-        self.completion_queue = gevent.queue.Queue()
-        self.callback_queue = gevent.queue.Queue()
-        self.session_queue = gevent.queue.Queue()
+        self.completion_queue = Queue()
+        self.callback_queue = Queue()
+        self.session_queue = Queue()
         self._running = True
 
         # Spawn our worker greenlets, we have
@@ -51,10 +81,12 @@ class SequentialGeventHandler(object):
     def _create_greenlet_worker(self, queue):
         def greenlet_worker():
             while self._running:
+                # We timeout after 1 and repeat so that we can gracefully
+                # shutdown if self_running is set to false
                 try:
                     func = queue.get(timeout=1)
                     func()
-                except gevent.queue.Empty:
+                except Empty:
                     continue
         gevent.spawn(greenlet_worker)
 
@@ -66,3 +98,6 @@ class SequentialGeventHandler(object):
             self.session_queue.put(lambda: callback.func(*callback.args))
         else:
             self.callback_queue.put(lambda: callback.func(*callback.args))
+
+        # Wake gevent wait/gets
+        os.write(_core_pipe_write, '\0')
