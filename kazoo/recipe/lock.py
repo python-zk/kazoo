b8c86ea7158796bd98ae122f87d585d66d91913f
Ben Bangert <ben@groovie.org>
2012-07-25 15:48:12 -0700

    Fix doc about LOST vs SUSPENDED state.

--- kazoo/recipe/lock.py
+++ kazoo/recipe/lock.py
@@ -7,7 +7,7 @@ It's highly recommended to add a state listener with
 :meth:`~KazooClient.add_listener` and watch for
 :attr:`~KazooState.LOST` and :attr:`~KazooState.SUSPENDED` state
 changes and re-act appropriately. In the event that a
-:attr:`~KazooState.SUSPENDED` state occurs, its certain that the lock
+:attr:`~KazooState.LOST` state occurs, its certain that the lock
 has been lost.
 
 """
