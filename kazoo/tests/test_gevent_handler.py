02f97821532ff447f6bac106d14941350a9a1851
Hanno Schlichting <hanno@hannosch.eu>
2012-09-04 14:33:52 +0200

    complete coverage for gevent handler

--- kazoo/tests/test_gevent_handler.py
+++ kazoo/tests/test_gevent_handler.py
@@ -1,9 +1,11 @@
+import time
 import unittest
 
 from gevent.event import Event
 from nose.tools import eq_
 from nose.tools import raises
 
+from kazoo.exceptions import NoNodeError
 from kazoo.protocol.states import Callback
 from kazoo.testing import KazooTestCase
 
@@ -72,6 +74,33 @@ class TestGeventHandler(unittest.TestCase):
             raise h.timeout_exception("This is a timeout")
         testit()
 
+    def test_exception_in_queue(self):
+        h = self._makeOne()
+        h.start()
+        ev = Event()
+
+        def func():
+            ev.set()
+            raise ValueError('bang')
+
+        call1 = Callback('completion', func, ())
+        h.dispatch_callback(call1)
+        ev.wait()
+
+    def test_queue_empty_exception(self):
+        from gevent.queue import Empty
+        h = self._makeOne()
+        h.start()
+        ev = Event()
+
+        def func():
+            ev.set()
+            raise Empty()
+
+        call1 = Callback('completion', func, ())
+        h.dispatch_callback(call1)
+        ev.wait()
+
     def test_peeking(self):
         from kazoo.handlers.gevent import _PeekableQueue
         from kazoo.handlers.gevent import Empty
@@ -94,17 +123,45 @@ class TestGeventClient(KazooTestCase):
     def test_start(self):
         client = self._get_client(handler=self._makeOne())
         client.start()
-        assert client.state == 'CONNECTED'
+        self.assertEqual(client.state, 'CONNECTED')
+        client.stop()
+
+    def test_start_stop_double(self):
+        client = self._get_client(handler=self._makeOne())
+        client.start()
+        self.assertEqual(client.state, 'CONNECTED')
+        client.handler.start()
+        client.handler.stop()
         client.stop()
 
     def test_basic_commands(self):
         client = self._get_client(handler=self._makeOne())
         client.start()
-        try:
-            assert client.state == 'CONNECTED'
-            client.create('/anode', 'fred')
-            eq_(client.get('/anode')[0], 'fred')
-            eq_(client.delete('/anode'), True)
-            eq_(client.exists('/anode'), None)
-        finally:
-            client.stop()
+        self.assertEqual(client.state, 'CONNECTED')
+        client.create('/anode', 'fred')
+        eq_(client.get('/anode')[0], 'fred')
+        eq_(client.delete('/anode'), True)
+        eq_(client.exists('/anode'), None)
+        client.stop()
+
+    def test_failures(self):
+        client = self._get_client(handler=self._makeOne())
+        client.start()
+        self.assertRaises(NoNodeError, client.get, '/none')
+        client.stop()
+
+    def test_data_watcher(self):
+        client = self._get_client(handler=self._makeOne())
+        client.start()
+        client.ensure_path('/some/node')
+        ev = Event()
+
+        @client.DataWatch('/some/node')
+        def changed(d, stat):
+            ev.set()
+
+        ev.wait()
+        ev.clear()
+        client.set('/some/node', 'newvalue')
+        ev.wait()
+        client.stop()
