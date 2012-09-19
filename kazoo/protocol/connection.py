bf5e1a6db88a26d91cbc9db53dac70e69df4c80d
Hanno Schlichting <hanno@hannosch.eu>
2012-09-19 12:52:11 +0200

    try dealing with `encode('hex')` again

--- kazoo/protocol/connection.py
+++ kazoo/protocol/connection.py
@@ -6,6 +6,7 @@ import select
 import socket
 import sys
 import time
+from binascii import hexlify
 from contextlib import contextmanager
 
 from kazoo.exceptions import (
@@ -47,6 +48,7 @@ PING_XID = -2
 AUTH_XID = -4
 
 if sys.version_info > (3, ):
+
     def buffer(obj, offset=0):
         return memoryview(obj)[offset:]
 
@@ -473,7 +475,7 @@ class ConnectionHandler(object):
         if self.log_debug:
             log.debug('    Using session_id: %r session_passwd: 0x%s',
                       client._session_id,
-                      client._session_passwd.encode('hex'))
+                      hexlify(client._session_passwd))
 
         with socket_error_handling():
             self._socket.connect((host, port))
@@ -504,7 +506,7 @@ class ConnectionHandler(object):
                       '    negotiated session timeout: %s\n'
                       '    connect timeout: %s\n'
                       '    read timeout: %s', client._session_id,
-                      client._session_passwd.encode('hex'),
+                      hexlify(client._session_passwd),
                       negotiated_session_timeout, connect_timeout,
                       read_timeout)
 
