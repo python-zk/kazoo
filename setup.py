5947457fb555eadc5d3edf8b6223abbf0f6c24da
Hanno Schlichting <hanno@hannosch.eu>
2012-09-21 15:07:35 +0200

    claim compatibility with Python 3.2

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
         "Topic :: Communications",
         "Topic :: System :: Distributed Computing",
         "Topic :: System :: Networking",
