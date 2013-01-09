620e03d329d6ed007630364e6114cee7836911f4
Hanno Schlichting <hanno@hannosch.eu>
2013-01-09 13:25:48 +0100

    one more case, of us being too fast now

--- kazoo/tests/test_watchers.py
+++ kazoo/tests/test_watchers.py
@@ -111,7 +111,6 @@ class KazooDataWatcherTests(KazooTestCase):
         update.clear()
 
         self.expire_session()
-        eq_(update.is_set(), False)
         self.client.retry(self.client.set, self.path, b'fred')
         update.wait(25)
         eq_(data[0], b'fred')
