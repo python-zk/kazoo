575d8ae9c0a1258d75785b2de40f4a52e6b361fb
rsegales <rsegales@twitter.com>
2013-10-24 14:32:06 -0700

    Add a ZK shell
    This is pretty much a handy clone of zkCli.sh that I use for the
    same things (but I prefer to use a Kazoo based one - since it's
    what the clients I deal with are using).
    
    It supports the basic ops:
    
    python2.7 contrib/shell.py localhost:2181
    (CONNECTED) /> ls
    zookeeper
    (CONNECTED) /> create foo 'bar'
    (CONNECTED) /> get foo
    bar
    (CONNECTED) /> cd foo
    (CONNECTED) /foo> create ish 'barish'
    (CONNECTED) /foo> cd ..
    (CONNECTED) /> ls foo
    ish
    (CONNECTED) /> create temp- 'temp' true true
    (CONNECTED) /> ls
    zookeeper foo temp-0000000001
    (CONNECTED) /> rmr foo
    (CONNECTED) />
    (CONNECTED) /> tree
    .
    ├── zookeeper
    │   ├── config
    │   ├── quota
    
    readline support is enabled (if readline is available).
    
    You can also copy individual files your local filesystem to a znode
    in a ZK host. Recursively copying from your filesystem to ZK is supported,
    but not from ZK to your local filesystem (since znodes can have content and
    children). Copying to everything under a znode to JSON would be fun (for
    backups and such).
    
    (CONNECTED) /> cp /etc/passwd zk://localhost:2181/passwd
    (CONNECTED) /> get passwd
    (...)
    unbound:x:992:991:Unbound DNS resolver:/etc/unbound:/sbin/nologin
    haldaemon:x:68:68:HAL daemon:/:/sbin/nologin
    
    Copying from one ZK cluster to another is supported, too:
    
    (CONNECTED) /> cp zk://localhost:2181/passwd zk://othercluster:2183/mypasswd

---
+++ contrib/shell/copy.py
@@ -0,0 +1,242 @@
+# helpers to move files/dirs to and from ZK and also among ZK clusters
+
+from __future__ import print_function
+
+from collections import namedtuple
+import os
+import re
+import urlparse
+
+from kazoo.client import KazooClient
+
+
+DEFAULT_ZK_PORT = 2181
+
+
+def zk_client(host, username, password):
+    if not re.match(":\d+$", host):
+        hostname = "%s:%d" % (host, DEFAULT_ZK_PORT)
+
+    client = KazooClient(hosts=host)
+    client.start()
+    return client
+
+
+class Netloc(namedtuple("Netloc", "username password host")):
+    @classmethod
+    def from_string(cls, netloc_string):
+        username = password = host = ""
+        if not "@" in netloc_string:
+            host = netloc_string
+        else:
+            username_passwd, host =  netloc.split("@")
+            if ":" in username_passwd:
+                username, password = username_passwd.split(":", 1)
+            else:
+                username = username_passwd
+
+        return cls(username, password, host)
+
+
+scheme_registry = {}
+def scheme_handler(scheme):
+    def class_wrapper(cls):
+        scheme_registry[scheme] = cls
+        return cls
+    return class_wrapper
+
+
+class CopyError(Exception): pass
+
+
+class Proxy(object):
+
+    def __init__(self, parse_result, exists):
+        self.parse_result = parse_result
+        self.netloc = Netloc.from_string(parse_result.netloc)
+        self.exists = exists
+
+    @property
+    def scheme(self):
+        return self.parse_result.scheme
+
+    @property
+    def url(self):
+        return self.parse_result.geturl()
+
+    @property
+    def path(self):
+        return self.parse_result.path
+
+    @property
+    def host(self):
+        return self.netloc.host
+
+    @property
+    def username(self):
+        return self.netloc.username
+
+    @property
+    def password(self):
+        return self.netloc.password
+
+    def set_url(self, string):
+        """ useful for recycling a stateful proxy """
+        self.parse_result = Proxy.parse(string)
+
+    @classmethod
+    def from_string(cls, string, exists):
+        """
+        if exists is bool, then check it either exists or it doesn't.
+        if exists is None, we don't care.
+        """
+        result = cls.parse(string)
+
+        if result.scheme not in scheme_registry:
+            raise CopyError("Invalid scheme: %s" % (result.scheme))
+
+        return scheme_registry[result.scheme](result, exists)
+
+    @classmethod
+    def parse(cls, url_string):
+        """ default to file:// """
+        if not re.match("^\w+://", url_string):
+            url_string = "file://%s" % (url_string)
+
+        return urlparse.urlparse(url_string)
+
+    def check_path(self):
+        raise NotImplementedError, "check_path must be implemented"
+
+    def read_path(self):
+        raise NotImplementedError, "read_path must be implemented"
+
+    def write_path(self, content):
+        raise NotImplementedError, "write_path must be implemented"
+
+    def children_of(self):
+        raise NotImplementedError, "children_of must be implemented"
+
+
+@scheme_handler("zk")
+class ZKProxy(Proxy):
+    """ read/write ZooKeeper paths """
+
+    def __init__(self, parse_result, exists):
+        super(ZKProxy, self).__init__(parse_result, exists)
+
+        self.client = zk_client(self.host, self.username, self.password)
+
+        if exists is not None:
+            self.check_path()
+
+    def check_path(self):
+        retval = True if self.client.exists(self.path) else False
+        if retval is not self.exists:
+            if exists:
+                m = "znode %s in %s doesn't exist" % \
+                    (self.path, self.host)
+            else:
+                m = "znode %s in %s exists" % (self.path, self.host)
+            raise CopyError(m)
+
+    def read_path(self):
+        return self.client.get(self.path)[0]
+
+    def write_path(self, content):
+        if self.client.exists(self.path):
+            self.client.set(self.path, content)
+        else:
+            self.client.create(self.path, content, makepath=True)
+
+    def children_of(self):
+        return self.zk_walk(self.path, "")
+
+    def zk_walk(self, root_path, path):
+        paths = []
+        full_path = "%s/%s" % (root_path, path) if path != "" else root_path
+        for c in self.client.get_children(full_path):
+            child_path = "%s/%s" % (path, c) if path != "" else c
+            paths.append(child_path)
+            paths += self.zk_walk(root_path, child_path)
+        return paths
+
+
+@scheme_handler("file")
+class FileProxy(Proxy):
+    def __init__(self, parse_result, exists):
+        super(FileProxy, self).__init__(parse_result, exists)
+
+        if exists is not None:
+            self.check_path()
+
+    def check_path(self):
+        if os.path.exists(self.path) is not self.exists:
+            if exists:
+                m = "Path %s doesn't exist" % (self.path)
+            else:
+                m = "Path %s exists" % (self.path)
+            raise CopyError(m)
+
+    def read_path(self):
+        if os.path.isfile(self.path):
+            with open(self.path, "r") as fp:
+                return "".join(fp.readlines())
+        elif os.path.isdir(self.path):
+            return ""
+        else:
+            raise CopyError("%s is of unknown file type" % (self.path))
+
+    def write_path(self, content):
+        """ this will overwrite dst path - be careful """
+
+        parent_dir = os.path.dirname(self.path)
+        try:
+            os.makedirs(parent_dir)
+        except OSError as ex:
+            pass
+        with open(self.path, "w") as fp:
+            fp.write(content)
+
+    def children_of(self):
+        root_path = self.path[0:-1] if self.path.endswith("/") else self.path
+        all = []
+        for path, dirs, files in os.walk(root_path):
+            path = path.replace(root_path, "")
+            if path.startswith("/"):
+                path = path[1:]
+            if path != "":
+                all.append(path)
+            for f in files:
+                all.append("%s/%s" % (path, f) if path != "" else f)
+        return all
+
+
+def do_copy(src, dst, verbose=False):
+    if verbose:
+        print("Copying from %s to %s" % (src.geturl(), dst.geturl()))
+
+    try:
+        dst.write_path(src.read_path())
+    except Exception as ex:
+        raise CopyError("Failed to copy: %s" % (str(ex)))
+
+
+def copy(src_url, dst_url, recursive=False, overwrite=False, verbose=False):
+    """
+    src and dst can be any either file://<path> or zk://[user:passwd@]host/<path>
+    """
+    src = Proxy.from_string(src_url, True)
+    dst = Proxy.from_string(dst_url, None if overwrite else False)
+
+    if not recursive:
+        do_copy(src, dst, verbose)
+    else:
+        if src.scheme == "zk" and dst.scheme == "file":
+            raise CopyError("Recursive copy from zk to fs isn't supported")
+
+        children = src.children_of()
+        for c in children:
+            src.set_url("%s/%s" % (src_url, c))
+            dst.set_url("%s/%s" % (dst_url, c))
+            do_copy(src, dst, verbose)
