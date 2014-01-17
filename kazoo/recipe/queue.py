6247cea1dff7dbbfe0fa467c1a2150d50d35138b
spankme <spankthespam@gmail.com>
2014-01-17 21:59:59 +0100

    New release method added
    New release method added to remove the lock from currently processed element without consuming the element (useful in case of errors during processing the data from the message, so that the message could be processed by some other queue consumers once it is released).

--- kazoo/recipe/queue.py
+++ kazoo/recipe/queue.py
@@ -248,6 +248,23 @@ class LockingQueue(BaseQueue):
             return True
         else:
             return False
+            
+    def release(self):
+        """Removes the lock from currently processed item without consuming it.
+
+        :returns: True if the lock was removed successfully, False otherwise.
+        :rtype: bool
+        """
+        if not self.processing_element is None and self.holds_lock:
+            id_, value = self.processing_element
+            with self.client.transaction() as transaction:
+                transaction.delete("{path}/{id}".format(
+                    path=self._lock_path,
+                    id=id_))
+            self.processing_element = None
+            return True
+        else:
+            return False
 
     def _inner_get(self, timeout):
         flag = self.client.handler.event_object()
