b038a0f2774a966d45390961789929caa6c76e15
Hanno Schlichting <hanno@hannosch.eu>
2015-05-28 11:41:26 +0200

    Make use of the new travis-ci caching mechanism.

--- ensure-zookeeper-env.sh
+++ ensure-zookeeper-env.sh
@@ -3,9 +3,9 @@
 set -e
 
 HERE=`pwd`
-ZOO_BASE_DIR="$HERE/bin/"
+ZOO_BASE_DIR="$HERE/zookeeper"
 ZOOKEEPER_VERSION=${ZOOKEEPER_VERSION:-3.4.6}
-ZOOKEEPER_PATH="$ZOO_BASE_DIR/zookeeper-$ZOOKEEPER_VERSION"
+ZOOKEEPER_PATH="$ZOO_BASE_DIR/$ZOOKEEPER_VERSION"
 ZOO_MIRROR_URL="http://apache.osuosl.org/"
 
 
@@ -13,6 +13,7 @@ function download_zookeeper(){
     mkdir -p $ZOO_BASE_DIR
     cd $ZOO_BASE_DIR
     curl --silent -C - $ZOO_MIRROR_URL/zookeeper/zookeeper-$ZOOKEEPER_VERSION/zookeeper-$ZOOKEEPER_VERSION.tar.gz | tar -zx
+    mv zookeeper-$ZOOKEEPER_VERSION $ZOOKEEPER_VERSION
     chmod a+x $ZOOKEEPER_PATH/bin/zkServer.sh
 }
 
