36fb60bbe20846a4d93ac5c2bcdc25465ced3132
Khaled Basbous <khaled@sixsq.com>
2018-09-21 12:01:07 +0200

    Update queue.py

--- kazoo/recipe/queue.py
+++ kazoo/recipe/queue.py
@@ -345,7 +345,9 @@ class LockingQueue(BaseQueue):
                 "{path}/{id}".format(path=self._entries_path, id=id_))
         except NoNodeError:
             # Item is already consumed
-            self.client.delete,
-                "{path}/{id}".format(path=self._lock_path, id=id_)
+            self.client.delete(
+                "{path}/{id}".format(
+                    path=self._lock_path,
+                    id=id_))
             return None
         return (id_, value)
