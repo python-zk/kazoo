5851e406f578cfb4dc08cf7cdd66d6575cb41ef8
Alex Ungurianu <alex.ungurianu@protonmail.com>
2022-10-28 22:34:21 +0100

    test: Don't throw when test fail while cluster is down

--- kazoo/tests/conftest.py
+++ kazoo/tests/conftest.py
@@ -4,7 +4,8 @@ log = logging.getLogger(__name__)
 
 
 def pytest_exception_interact(node, call, report):
-    cluster = node._testcase.cluster
-    log.error("Zookeeper cluster logs:")
-    for logs in cluster.get_logs():
-        log.error(logs)
+    if hasattr(node._testcase, "cluster"):
+        cluster = node._testcase.cluster
+        log.error("Zookeeper cluster logs:")
+        for logs in cluster.get_logs():
+            log.error(logs)
