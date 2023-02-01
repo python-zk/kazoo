e77eb05aded8072d77ca65592912a3fd9b5f1a41
Alex Ungurianu <alex.ungurianu@protonmail.com>
2023-02-01 00:31:53 +0000

    fix(tests): Be more lenient when handling pytest exceptions

--- kazoo/tests/conftest.py
+++ kazoo/tests/conftest.py
@@ -4,7 +4,7 @@ log = logging.getLogger(__name__)
 
 
 def pytest_exception_interact(node, call, report):
-    if hasattr(node._testcase, "cluster"):
+    if hasattr(node, "_testcase") and hasattr(node._testcase, "cluster"):
         cluster = node._testcase.cluster
         log.error("Zookeeper cluster logs:")
         for logs in cluster.get_logs():
