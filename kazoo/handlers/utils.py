2dc4ff826150f4543d3a3bc37c60308b708067ea
jessesightler-redhat <168553759+jessesightler-redhat@users.noreply.github.com>
2024-07-04 21:14:06 -0400

    Update exception message in kazoo/handlers/utils.py
    Co-authored-by: Jeff Widman <jeff@jeffwidman.com>

--- kazoo/handlers/utils.py
+++ kazoo/handlers/utils.py
@@ -241,8 +241,7 @@ def create_tcp_connection(
             context.load_default_certs(ssl.Purpose.SERVER_AUTH)
             if check_hostname and not verify_certs:
                 raise ValueError(
-                    "Error, if check_hostname is specified "
-                    + "verify_certs must be False"
+                    "verify_certs must be False when check_hostname is specified"
                 )
             # We must set check_hostname to False prior to setting
             # verify_mode to CERT_NONE.
